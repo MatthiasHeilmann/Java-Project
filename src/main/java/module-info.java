1fe0863c23705290aae1c2cd251898545db90a5a
Ynfiniti <sean_schwarz@gmx.de>
2022-06-21 19:13:46 +0200

    Redflag begone thot

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ module com.example.javaproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+	requires mysql.connector.java;
 
 
-    opens com.example.javaproject to javafx.fxml;
+	opens com.example.javaproject to javafx.fxml;
     exports com.example.javaproject;
 
     opens com.example.javaproject.Tables to javafx.fxml;
